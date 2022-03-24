0403292e3507f830d5f9940f4b374dddc27cf88d
bowenlin1101 <bowenblinblue@gmail.com>
2022-03-24 18:20:49 -0400

    initial commit

--- contentScript.js
+++ contentScript.js
@@ -10,7 +10,6 @@ chrome.runtime.onMessage.addListener((request) => {
         for (i of document.getElementsByTagName("button")){
             for (j of i.childNodes){
                 if (j.tagName == "SPAN" && j.innerHTML == "Join"){
-                    console.log("ran")
                     document.getElementById("i3").value = request.code
                     code = request.code
                     document.getElementById("i3").focus()
@@ -22,7 +21,6 @@ chrome.runtime.onMessage.addListener((request) => {
 })
 
 var announceInterval = setInterval(() => {
-    console.log(code)
     chrome.storage.sync.get(["code"], (result) => {
         if (window.location.href.includes(result.code)){
             alert("Meet is Ready!")
